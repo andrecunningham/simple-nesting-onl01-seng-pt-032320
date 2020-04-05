c12d4e77284c9e3de4ba313fa2371ffdb0dc18f3
Andre Cunningham <andrecunningham62@gmail.com>
2020-04-05 20:12:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,7 +129,9 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-        programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+       
      }
-
+ programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+ 
+ programmer_hash
 end
